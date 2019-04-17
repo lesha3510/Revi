9660855e8498ea9b52995eb3f1af1e5d66c636b9
lesha3510 <35871412+lesha3510@users.noreply.github.com>
2019-04-17 17:03:44 +0300

    adaptiv3

--- Promstroy35/script.js
+++ Promstroy35/script.js
@@ -2,3 +2,8 @@ document.querySelector('.hamburger').addEventListener('click', function(e){
     e.preventDefault();
     this.classList.toggle('is-active');
 })
+
+        $(document).ready(function(){
+          $('.slick-slider').slick({
+          });
+        });
