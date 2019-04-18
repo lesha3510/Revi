3d0618575a78e56d72fcf7ed3269eff80af6073b
lesha3510 <lesha35105@mail.ru>
2019-04-18 22:20:45 +0300

    menu_action

--- Promstroy35/js/script.js
+++ Promstroy35/js/script.js
@@ -1,11 +1,15 @@
-$(document).ready(function(){
-    var humburger = document.querySelector('.hamburger');
-
-    humburger.onclick = function(e){
-        console.log(humburger);
+document.querySelector('.hamburger').addEventListener('click',function(e){
         e.preventDefault();
-        humburger.classList.toggle('is-active');
-    }
+//        humburger.classList.toggle('is-active');
+        if(this.classList.contains('is-active')){
+            this.classList.remove('is-active')
+            document.querySelector('#menu').classList.remove('nav-active')
+        }
+        else {
+            this.classList.add('is-active')
+            document.querySelector('#menu').classList.add('nav-active')
+        }
+    })
+
     $('.slick-slider').slick({
     });
-});
